80b95e2c23449d4c84a38139c8eb091f1cdafef0
Antti <antti.lahnaoja@ac2sg.fi>
2018-02-21 13:55:10 +0200

    Fix soap message pretty printer

--- ocpp-v1_6/src/main/java/eu/chargetime/ocpp/PrettyHelper.java
+++ ocpp-v1_6/src/main/java/eu/chargetime/ocpp/PrettyHelper.java
@@ -1,12 +1,14 @@
 package eu.chargetime.ocpp;
 
 /*
-    This is pretty printer for the SOAP xml messages.
-    Used to log SOAP message content in a bit more readable form, could be better.
+    This class is pretty printer for the SOAP xml messages.
+    Used to log SOAP message content in a bit more readable form, could be better though
  */
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import java.io.ByteArrayOutputStream;
 import java.io.StringWriter;
 import javax.xml.soap.SOAPMessage;
 import javax.xml.transform.OutputKeys;
@@ -14,6 +16,7 @@ import javax.xml.transform.Source;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.stream.StreamResult;
+
 
 public class PrettyHelper {
     private static final Logger logger = LogManager.getLogger(PrettyHelper.class);
@@ -24,23 +27,35 @@ public class PrettyHelper {
             Transformer tf = tff.newTransformer();
 
             // Set formatting
-            tf.setOutputProperty(OutputKeys.METHOD, "xml");
-            tf.setOutputProperty(OutputKeys.STANDALONE, "yes");
+            //tf.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
+            //tf.setOutputProperty(OutputKeys.METHOD, "xml");
+            //tf.setOutputProperty(OutputKeys.STANDALONE, "yes");
             tf.setOutputProperty(OutputKeys.INDENT, "yes");
-            tf.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
             tf.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2");
-
+            tf.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
             Source sc = soapMessage.getSOAPPart().getContent();
             StringWriter stringWriter = new StringWriter();
             StreamResult result = new StreamResult(stringWriter);
             tf.transform(sc, result);
-
             return result.getWriter().toString();
         } catch (Exception e) {
-            logger.warn("Exception in getSOAPMessageAsString " + e.getMessage());
-            return null;
+            logger.warn("Error when making xml string " + e.getMessage());
+            return "Error when making xml string";
         }
-
     }
-}
 
+    public static String getSOAPMessageAsRow(SOAPMessage soapMessage) {
+        /*
+            Another version about pretty printer, shorter but not better...
+         */
+        try {
+            ByteArrayOutputStream s = new ByteArrayOutputStream();
+            soapMessage.writeTo(s);
+            return s.toString();
+        } catch (Exception ex) {
+            logger.warn("Error converting to String ", ex);
+            return "Error converting to String";
+        }
+    }
+
+}
